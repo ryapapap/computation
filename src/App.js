c52075c4dfa21a3dc7bbe339905cec8c5387427b
Ryan Patterson <ryan@Ryans-MacBook-Pro.local>
2020-07-30 18:10:11 -0700

    just silly yo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {useTransition, animated} from 'react-spring';
 import './App.css';
 
 function App() {
+  const [show, setShow] = useState(false);
+  const transitions = useTransition(show, null, {
+    from: { 
+      opacity: 1,
+      transform: 'scale(0)' },
+    enter: { opacity: 0, transform: 'scale(1)', },
+    leave: { opacity: 0, },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello, I am a philosopher poet who works with computation and interaction. 
+      <br /><br />
+      Wanna see?
+      2+2 = {show ? 4 : <button onClick={() => setShow(true)}>?</button>}
+      {transitions.map(({ item, props, key }) =>
+        item && <>
+          <div className="anim one"><animated.div style={props}>woaw</animated.div></div>
+          <div className="anim two"><animated.div style={props}>amazing</animated.div></div>
+        </>
+      )}
+      
     </div>
   );
 }
